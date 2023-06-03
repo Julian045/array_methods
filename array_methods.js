741137c97b4cf5c4c379e082525e7ca69f8b0b2a
Julian <22413046+Julian045@users.noreply.github.com>
2023-06-03 20:10:17 +0200

    Update array_methods.js
    the shift, unshift, pop, and push methods use the concat() method to combine arrays

--- array_methods.js
+++ array_methods.js
@@ -1,28 +1,24 @@
 //ARRAY METHODS
 
 function shift(arr) {
-    const [first, ...rest] = arr;
-    return rest;
+  return arr.slice(1);
 }
 
 function unshift(arr, ...rest) {
-    return [...rest, ...arr];
+  return rest.concat(arr);
 }
 
 function pop(arr) {
-    const {
-        [arr.length -1]: last, ...rest
-    } = arr;
-    return Object.values(rest);
+  return arr.slice(0, -1);
 }
 
 function push(arr, ...rest) {
-    return [...arr, ...rest];
+  return arr.concat(rest);
 }
 
-const input = [1, 2, 3, 4 ,5];
+const input = [1, 2, 3, 4, 5];
 
-console.log(shift(input));            //[2, 3, 4, 5]
-console.log(unshift(input, 6, 7, 8)); //[6, 7, 8, 1, 2, 3, 4, 5]
-console.log(pop(input));              //[1, 2, 3, 4]
-console.log(push(input, 6, 7, 8));    //[1, 2, 3, 4, 5, 6, 7, 8]
+console.log(shift(input));            // [2, 3, 4, 5]
+console.log(unshift(input, 6, 7, 8));  // [6, 7, 8, 1, 2, 3, 4, 5]
+console.log(pop(input));              // [1, 2, 3, 4]
+console.log(push(input, 6, 7, 8));    // [1, 2, 3, 4, 5, 6, 7, 8]
